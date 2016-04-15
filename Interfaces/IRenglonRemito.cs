3fc3edf298a6590430e482ada0f1137cabc39024
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2016-04-15 13:44:18 -0300

    Mejorada estructura de carpetas

---
+++ Interfaces/IRenglonRemito.cs
@@ -0,0 +1,21 @@
+﻿namespace Hamekoz.Negocio
+{
+	interface IRenglonRemito
+	{
+		Articulo Articulo { get; set; }
+
+		double Cantidad { get; set; }
+
+		double Costo { get; set; }
+
+		long LoteId { get; set; }
+
+		long RemitoId { get; set; }
+
+		int NumeroRenglon { get; set; }
+
+		double PrecioTotal { get; }
+
+		double PrecioUnitario { get; set; }
+	}
+}
