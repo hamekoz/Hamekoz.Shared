f842829d7c9407a8277c7040e1d35a7a56c6e1ce
Ezequiel Taranto <ezequiel89@gmail.com>
2016-07-15 11:16:25 -0300

    add extension FirstDateOfWeek
    Signed-off-by: Ezequiel Taranto <ezequiel89@gmail.com>

--- Extensions/DateTimeExtensions.cs
+++ Extensions/DateTimeExtensions.cs
@@ -80,5 +80,22 @@ namespace Hamekoz.Extensions
 			value = value.AddDays (-1);
 			return value;
 		}
+
+		public static DateTime FirstDateOfWeekISO8601 (int year, int weekOfYear)
+		{
+			DateTime jan1 = new DateTime (year, 1, 1);
+			int daysOffset = DayOfWeek.Thursday - jan1.DayOfWeek;
+
+			DateTime firstThursday = jan1.AddDays (daysOffset);
+			var cal = CultureInfo.CurrentCulture.Calendar;
+			int firstWeek = cal.GetWeekOfYear (firstThursday, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+
+			var weekNum = weekOfYear;
+			if (firstWeek <= 1) {
+				weekNum -= 1;
+			}
+			var result = firstThursday.AddDays (weekNum * 7);
+			return result.AddDays (-3);
+		}
 	}
 }
