749b425ef39fafbb4485d05c8fe6f31cd9b980ed
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2017-04-18 10:20:11 -0300

    Eliminada listas de telefonos en favor de un unico telefono primario y una lista de contactos
    Signed-off-by: Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>

--- Clases/Cliente.cs
+++ Clases/Cliente.cs
@@ -18,9 +18,7 @@
 //
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 using System;
-using System.Collections.Generic;
 using Hamekoz.Argentina;
 using Hamekoz.Core;
 using Hamekoz.Fiscal;
@@ -81,13 +79,11 @@ namespace Hamekoz.Negocio
 			set;
 		}
 
-		//TODO deberia ser un unico telefono y tener una lista de contactos
-		public IList<Telefono> Telefonos {
+		public string Telefono {
 			get;
 			set;
 		}
 
-		//UNDONE aca podria ser una lista de correos electronicos
 		public string Email {
 			get;
 			set;
