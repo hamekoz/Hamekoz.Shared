69b69364867620b5627615530769b3f147e58e88
Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>
2017-12-05 12:42:53 -0300

    Clase marcada como IPersistible
    Signed-off-by: Claudio Rodrigo Pereyra Diaz <claudiorodrigo@pereyradiaz.com.ar>

--- Clases/SucursalDeBanco.cs
+++ Clases/SucursalDeBanco.cs
@@ -18,14 +18,11 @@
 //
 //  You should have received a copy of the GNU Lesser General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-using System.Collections.Generic;
 using Hamekoz.Core;
 
 namespace Hamekoz.Negocio
 {
-
-	public class SucursalDeBanco : IIdentifiable
+	public class SucursalDeBanco : IIdentifiable, IPersistible
 	{
 		#region IIdentifiable implementation
 
